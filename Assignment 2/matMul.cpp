476e1048794cc7b779c4747839e3c9fe8cf81f10
Indra <ikhatri_be22@thapar.edu>
2025-02-05 16:01:03 +0530

    completed matrix multiplication

--- Assignment 2/matMul.cpp
+++ Assignment 2/matMul.cpp
@@ -21,47 +21,52 @@ void matMul(vector<double> &a,vector<double> &b,vector<double> &c,int rows_per_p
 int main(){
 
     MPI_Init(NULL,NULL);
+    Timer t;
 
     int rank,size;
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    vector<double> a,b,c;
+    vector<double> a,b(dims*dims),c;
     vector<double> local_a,local_c;
     int rows_per_process = dims/size;
 
     if(rank == 0){
+        srand(time(NULL));
         a.resize(dims*dims);
-        b.resize(dims*dims);
         c.resize(dims*dims);
 
         for(int i = 0;i<dims;i++){
             for(int j = 0;j<dims;j++){
-                a[i*dims + j] = (double) rand();
-                b[i*dims + j] = (double) rand();
+                a[i*dims + j] = (double)rand();
+                b[i*dims + j] = (double)rand();
             }
         }
     }
 
     local_a.resize(rows_per_process * dims);
     local_c.resize(rows_per_process * dims);
-    MPI_Scatter((void *)a.data(),rows_per_process * dims,MPI_BYTE,(void*)local_a.data(),rows_per_process*dims,MPI_BYTE,0,MPI_COMM_WORLD);
+    MPI_Scatter(a.data(),rows_per_process * dims,MPI_DOUBLE,local_a.data(),rows_per_process*dims,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
-    MPI_Bcast((void*)b.data(),b.size(),MPI_BYTE,0,MPI_COMM_WORLD);
-    MPI_Recv((void*)b.data(),b.size(),MPI_BYTE,0,MPI_ANY_TAG,MPI_COMM_WORLD,NULL);  
+    MPI_Bcast(b.data(),b.size(),MPI_DOUBLE,0,MPI_COMM_WORLD);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == 0) t.tick();
 
     matMul(local_a,b,local_c,rows_per_process);
 
-    MPI_Gather((void*)local_c.data(),rows_per_process*dims,MPI_BYTE,(void*)c.data(),rows_per_process*dims,MPI_BYTE,0,MPI_COMM_WORLD);
-
+    MPI_Barrier(MPI_COMM_WORLD);    
+    if(rank == 0) t.tock();
+    MPI_Gather(local_c.data(),rows_per_process*dims,MPI_DOUBLE,c.data(),rows_per_process*dims,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    
     if(rank == 0){
-        cout<<"OUTPUT MATRIX\n";
-        for(int i = 0;i<dims;i++){
-            for(int j = 0;j<dims;j++){
-                cout<<c[i*dims + j]<<" ";
-            }
-            cout<<endl;
-        }
+        cout<<"PARTIAL COMPUTATION TIME -> "<<t.time()<<endl;
+        t.tick();
+        matMul(a,b,c,dims);
+        t.tock();
+        cout<<"SERIAL COMPUTATION TIME -> "<<t.time()<<endl;
     }
+
+    MPI_Finalize();
 
 }
