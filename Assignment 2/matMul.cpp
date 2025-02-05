e72447c3f00b965b0aa315c2aa0c63b42240dd39
Indra <ikhatri_be22@thapar.edu>
2025-02-05 15:21:27 +0530

    changes to matMul

--- Assignment 2/matMul.cpp
+++ Assignment 2/matMul.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<mpi.h>
 #include<timer.hpp>
+#include<vector>
 
 #define dims 70
 
 using namespace std;
 
-void matMul(vector<double> a,vector<double> b,vector<double> &c,int rank,int size){
-    for(int i = rank;i<dims;i+=(size-1)){
+void matMul(vector<double> &a,vector<double> &b,vector<double> &c,int rows_per_process){
+    for(int i = 0;i<rows_per_process;i++){
         for(int j = 0;j<dims;j++){
             c[i*dims + j] = 0;
             for(int k = 0;k<dims;k++){
@@ -18,8 +19,6 @@ void matMul(vector<double> a,vector<double> b,vector<double> &c,int rank,int siz
 }
 
 int main(){
-    vector<double> a(dims*dims);
-    vector<double> b(dims*dims);
 
     MPI_Init(NULL,NULL);
 
@@ -27,10 +26,42 @@ int main(){
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
+    vector<double> a,b,c;
+    vector<double> local_a,local_c;
+    int rows_per_process = dims/size;
+
     if(rank == 0){
-        MPI_Bcast((void *) a.data(),a.size(),MPI_BYTE,0,MPI_COMM_WORLD);
-        MPI_Bcast((void *) b.data(),b.size(),MPI_BYTE,0,MPI_COMM_WORLD);
+        a.resize(dims*dims);
+        b.resize(dims*dims);
+        c.resize(dims*dims);
+
+        for(int i = 0;i<dims;i++){
+            for(int j = 0;j<dims;j++){
+                a[i*dims + j] = (double) rand();
+                b[i*dims + j] = (double) rand();
+            }
+        }
     }
-    
+
+    local_a.resize(rows_per_process * dims);
+    local_c.resize(rows_per_process * dims);
+    MPI_Scatter((void *)a.data(),rows_per_process * dims,MPI_BYTE,(void*)local_a.data(),rows_per_process*dims,MPI_BYTE,0,MPI_COMM_WORLD);
+
+    MPI_Bcast((void*)b.data(),b.size(),MPI_BYTE,0,MPI_COMM_WORLD);
+    MPI_Recv((void*)b.data(),b.size(),MPI_BYTE,0,MPI_ANY_TAG,MPI_COMM_WORLD,NULL);  
+
+    matMul(local_a,b,local_c,rows_per_process);
+
+    MPI_Gather((void*)local_c.data(),rows_per_process*dims,MPI_BYTE,(void*)c.data(),rows_per_process*dims,MPI_BYTE,0,MPI_COMM_WORLD);
+
+    if(rank == 0){
+        cout<<"OUTPUT MATRIX\n";
+        for(int i = 0;i<dims;i++){
+            for(int j = 0;j<dims;j++){
+                cout<<c[i*dims + j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
 
 }
